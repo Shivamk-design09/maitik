2c31dd05c11ddccc7e4e4098170d05cf2a237478
shivam <shivamk44409@gmail.com>
2026-09-25 00:57:57 +0530

    auth

--- apps/http-backend/index.ts
+++ apps/http-backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express"
+const app = express()
+
+app.use(express.json())
+
+
+// defining the routes
+app.post("/api/v1/auth",authrout)
+
+app.listen(4000,()=>{
+console.log('code is running on port ', 4000)
+})
